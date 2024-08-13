1decd8426dfb9b5d9b4ae5f3d8039fbc6369663b
LucasBaccaro <lucas.ospegap@gmail.com>
2024-08-13 11:53:59 -0300

    detail implementation

---
+++ composeApp/src/commonMain/kotlin/baccaro/lucas/com/domain/usecase/GetCocktailDetailUseCase.kt
@@ -0,0 +1,11 @@
+package baccaro.lucas.com.domain.usecase
+
+import baccaro.lucas.com.data.remote.api.OperationResult
+import baccaro.lucas.com.domain.model.CocktailModel
+import baccaro.lucas.com.domain.repository.CocktailRepository
+
+class GetCocktailByIdUseCase(private val repository: CocktailRepository) {
+    suspend operator fun invoke(id: String): OperationResult<CocktailModel> {
+        return repository.getCocktailById(id)
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/baccaro/lucas/com/presentation/viewmodel/CocktailDetailViewModel.kt
@@ -0,0 +1,45 @@
+package baccaro.lucas.com.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import baccaro.lucas.com.data.remote.api.OperationResult
+import baccaro.lucas.com.domain.model.CocktailModel
+import baccaro.lucas.com.domain.usecase.GetCocktailByIdUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CocktailDetailViewModel(
+    private val getCocktailByIdUseCase: GetCocktailByIdUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CocktailDetailUiState())
+    val uiState: StateFlow<CocktailDetailUiState> = _uiState.asStateFlow()
+
+    fun getCocktailById(id: String) {
+        _uiState.update { it.copy(isLoading = true, error = null) }
+        viewModelScope.launch {
+            when (val result = getCocktailByIdUseCase(id)) {
+                is OperationResult.Success -> {
+                    _uiState.update {
+                        it.copy(isLoading = false, error = null, cocktail = result.data)
+                    }
+                }
+
+                is OperationResult.Error -> {
+                    _uiState.update {
+                        it.copy(isLoading = false, error = result.exception)
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class CocktailDetailUiState(
+    val cocktail: CocktailModel? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
